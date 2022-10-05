1bf74c5296594af34d16e81e14a89c6620d88fc8
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-10-05 02:31:25 +0100

    update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,27 +1,29 @@
 // reference
-// let Sender = document.forms["myForm"]["email"].value.trim();
-// let Message = document.forms["myForm"]["message"].value.trim();
-// let Title = document.forms["myForm"]["subject"].value.trim();
+let Sender = document.forms["myForm"]["email"].value.trim();
+let Message = document.forms["myForm"]["message"].value.trim();
+let Title = document.forms["myForm"]["subject"].value.trim();
 
-let Sender = document.getElementById("email").textContent.trim();
-let Message = document.getElementById("message").value.trim();
-let Title = document.getElementById("subject").value.trim();
+let code = "yfpifukyhxsvzrxr";
+
+// let Sender = document.getElementById("email").value.trim();
+// let Message = document.getElementById("message").value.trim();
+// let Title = document.getElementById("subject").value.trim();
 
 
 
 
 function sendEmail(){
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "oluwapelumiabiodun45@gmail.com",
-        Password : "7E668E6CD9EFC3CB9DB0CCD4F7A5AD5A91B5",
-        To : 'abiodunoluwapelumi859@gmail.com',
-        From : Sender,
-        Subject : Title,
-        Body : Message
+        SecureToken: "E8C6E0D7-22A4-43F8-9BE5-BC7B717F807D", 
+        To : 'oluwapelumiabiodun45@gmail.com',
+        From : "Amos",
+        Subject : "Title",
+        Body : "Message"
     }).then(
       message => alert("Sent successfully")
     );
+
+    
 
     
 }
